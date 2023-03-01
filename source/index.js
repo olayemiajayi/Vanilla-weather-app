463dc1cef2b8f45897420bc01d71b0083f1da4ea
Yetunde Ajayi <olayemioluwa17@gmail.com>
2023-02-28 17:00:41 -0700

    added the weather icon and temperature

--- source/index.js
+++ source/index.js
@@ -20,6 +20,7 @@ console.log(date.getHours());
 console.log(date.getMinutes());
 
 city.innerHTML = `${day}, ${hours}:${min}`;
+
 //search engine
 
 function displayTemperature(response) {
@@ -28,28 +29,58 @@ function displayTemperature(response) {
   console.log(response.data.temperature.humidity);
   console.log(response.data.wind.speed);
   console.log(response.data.condition.description);
-  console.log(response.data.condition.icon_url);
-  let temperature = Math.round(response.data.temperature.current);
+  console.log(response.data.condition.icon);
+
+  celsiusTemperature = response.data.temperature.current;
+
   let id = document.querySelector("#number");
-  id.innerHTML = `${temperature}`;
+  id.innerHTML = Math.round(celsiusTemperature);
   let weather = Math.round(response.data.wind.speed);
   let wind = document.querySelector("#wind-wind");
-  wind.innerHTML = `Wind:${weather} Km/h`;
+  wind.innerHTML = `Wind: ${weather}Km/h`;
   let humidity = document.querySelector("#wind-humidity");
-  humidity.innerHTML = `Humidity:${response.data.temperature.humidity}`;
+  humidity.innerHTML = `Humidity: ${response.data.temperature.humidity}`;
   let description = document.querySelector("#description-weather");
   description.innerHTML = `${response.data.condition.description}`;
-  let icon = document.querySelector("#icon");
-  icon.setAttribute =
-    ("src",
-    `http://shecodes-assets.s3.amazonaws.com/api/${response.data.condition.icon_url}few-clouds-day.png`);
+  let iconElement = document.querySelector("#icon-app");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
 }
-let city = document.querySelector("#country-weather");
-city.innerHTML = `${city.value}`;
-let apiKey = "308ee4b586fftbc5ce47ob29fd3f7a87";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=sydney&key=${apiKey}&units=metric`;
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
 
-let place = document.querySelector("#form-search");
-place.addEventListener("submit", search);
+function search(city) {
+  let apiKey = "308ee4b586fftbc5ce47ob29fd3f7a87";
+
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=308ee4b586fftbc5ce47ob29fd3f7a87&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function placeHome(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  search(cityInput.value);
+  let heading = document.querySelector("#city-name");
+  heading.innerHTML = `${cityInput.value}`;
+}
+let form = document.querySelector("#form-search");
+form.addEventListener("submit", placeHome);
+
+function currentTemperature(event) {
+  event.preventDefault();
+  let current = (celsiusTemperature * 9) / 5 + 32;
+  let temperature = document.querySelector("#number");
+  temperature.innerHTML = Math.round(current);
+}
+let celsiusTemperature = "null";
+let fahrenheit = document.querySelector("#degree-fahrenheit");
+fahrenheit.addEventListener("click", currentTemperature);
+
+function currentCelsius(event) {
+  event.preventDefault();
+  let temp = document.querySelector("#number");
+  temp.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsius = document.querySelector("#degree-celsius");
+celsius.addEventListener("click", currentCelsius);
